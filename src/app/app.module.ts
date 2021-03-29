a5e9effc269fc412f1fccb3385c2134c6674c796
jacobRinehart42 <jacobofmirkwood@yahoo.com>
2021-03-29 13:25:10 -0500

    3 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { GreetingComponent } from './greeting/greeting.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, GreetingComponent ],
+  declarations: [ AppComponent, HelloComponent, GreetingComponent, ButtonComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/greeting/greeting.component.ts
+++ src/app/greeting/greeting.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-greeting',
-  templateUrl: './greeting.component.html',
-  styleUrls: ['./greeting.component.css']
+  selector: "app-greeting",
+  template: `
+    <ng-content select="h1"></ng-content>
+    <ng-content select="app-button"></ng-content>
+    <h2>It is hot out there</h2>
+    <ng-content select="p"></ng-content>
+  `,
+  styleUrls: ["./greeting.component.css"]
 })
 export class GreetingComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
